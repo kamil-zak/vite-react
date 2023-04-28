327d9a3d32b40b4bbe6c38cb2756daa80f8ad201
Kamil Żak <kamil.zak.pl@outlook.com>
2023-04-28 17:40:30 +0200

    Configure vite pwa plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  manifest: {
+    name: 'PWA Application',
+    short_name: 'PwaApp',
+    theme_color: '#6B76C2',
+    background_color: '#FFFFFF',
+    display: 'standalone',
+    icons: [
+      {
+        src: 'pwa-icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-icons/icon-256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-icons/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}'],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(pwaOptions)],
   server: {
     host: true,
   },
